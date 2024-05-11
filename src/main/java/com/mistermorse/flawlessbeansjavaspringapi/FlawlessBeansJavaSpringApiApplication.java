88d09961544009745cc7855de7465628ecd0ee35
Matt Harp <matthewharp@gmail.com>
2024-05-11 14:30:33 -0500

    Initial working commit from Initializr.

---
+++ src/main/java/com/mistermorse/flawlessbeansjavaspringapi/FlawlessBeansJavaSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.mistermorse.flawlessbeansjavaspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlawlessBeansJavaSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlawlessBeansJavaSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mistermorse/flawlessbeansjavaspringapi/FlawlessBeansJavaSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mistermorse.flawlessbeansjavaspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlawlessBeansJavaSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
